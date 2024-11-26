f5c0f786d2f61e56be75112a0cac4c4cad90c2b9
Spencer Miller <spencerrmiller3@gmail.com>
2024-11-25 22:12:26 -0700

    working on products

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
   import Header from "../components/Header.svelte";
   import Hero from "../components/Hero.svelte";
+  import Product from "../components/Product.svelte";
 
 </script>
 <main class="flex flex-col">
-
     <Hero/>
+    <Product />
 </main>
